ed538915b38a54e67f9a9f5e26fef1daeaabc680
Sarpdoruk Tahmaz <sarpdoruk@gmail.com>
2024-01-08 18:14:27 +0300

    upgraded guzzlehttp

--- VP/Client.php
+++ VP/Client.php
@@ -54,25 +54,19 @@ class VP_Client {
         $request_method = $this->_request->get_method();
         $request_url = $this->_request->get_url();
         $send_params = array_merge($this->_default_params(), $this->_request->get_params());
-        $http_client = new \Guzzle\Http\Client();
-        $http_client->setSslVerification(true);
-        $http_client->getEventDispatcher()->addListener('request.error', function(\Guzzle\Common\Event $event) {
-            if ($event['response']->getStatusCode() != 200) {
-                $event->stopPropagation();
-            }
-        });
+        $http_client = new \GuzzleHttp\Client();
 
         $full_url = $this->_client_url().$request_url;
 
         if (in_array($request_method, array('put', 'post')))
         {
-            $response = $http_client->$request_method($full_url, array('content-type' => 'application/x-www-form-urlencoded'), $send_params);
+            $response = $http_client->$request_method($full_url, array('headers' => array('content-type' => 'application/x-www-form-urlencoded'), 'form_params' => $send_params));
         }
         else
         {
-            $response = $http_client->$request_method($full_url.'?'.http_build_query($send_params), array('content-type' => 'application/x-www-form-urlencoded'));
+            $response = $http_client->$request_method($full_url.'?'.http_build_query($send_params), array('headers' => array('content-type' => 'application/x-www-form-urlencoded')));
         }
-        return new VP_Response($response->send());
+        return new VP_Response($response);
     }
 
 }
